5b53edf0272d49305bfa3c318d5c0e9798b8ff73
PraveenBalaji003 <praveenmj003@gmail.com>
2026-04-15 16:28:42 +0530

    Objects And Arrays

---
+++ arrays/Arrays.java
@@ -0,0 +1,23 @@
+
+
+public class Arrays{
+    public static void main(String[] args){
+        // Array: used to store multiple values of the same data type
+        /*
+         * Arrays have a fixed size.
+         * Once an array is created (initialized), its size cannot be changed.
+        */
+        int arr1[] = {1,2,3,4,5};
+        System.out.println("First element of the array arr1 is - "+arr1[0]);
+        // Accessing the first element (index starts from 0)
+        arr1[0] = 0;
+        System.out.println("After Update, First element of the array arr1 is - "+arr1[0]);
+        System.out.println("=============================================");
+        int arr2[] = new int[100];
+        for(int i=0;i<100;i++){
+            arr2[i] = i+1;
+            System.out.println(arr2[i]);
+        }
+        System.out.println("=============================================");
+    }
+}
